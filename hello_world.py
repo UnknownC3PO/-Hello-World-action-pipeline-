347f509c8c1b1e08a4ae11ce1f0e41f3bef9b29f
UnknownC3PO <80990115+UnknownC3PO@users.noreply.github.com>
2021-08-13 00:19:14 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,20 +1,21 @@
 import json
 import urllib3
-base_id="app1yGHzBsgY0VVBd"
-table_name="MainTable"
-api_key="key4jDdoqZmxtIjxP"
-url = "https://api.airtable.com/v0/"+base_id+"/"+table_name+"?api_key="+api_key
-http = urllib3.PoolManager()
-r = http.request('GET', url)
-airtable_response = json.loads(r.data.decode('utf-8'))
-z=[]
-for i in airtable_response["records"]:
-    z.append(i["fields"]["title"])
-print(z)
-gen=[]
-for i in range(len(z)):
-    gen.append(z.pop(-1))
-    z.insert(0,gen[0])
+def lambda_handler(event, context):
+    base_id="app1yGHzBsgY0VVBd"
+    table_name="MainTable"
+    api_key="key4jDdoqZmxtIjxP"
+    url = "https://api.airtable.com/v0/"+base_id+"/"+table_name+"?api_key="+api_key
+    http = urllib3.PoolManager()
+    r = http.request('GET', url)
+    airtable_response = json.loads(r.data.decode('utf-8'))
+    z=[]
+    for i in airtable_response["records"]:
+        z.append(i["fields"]["title"])
+    print(z)
     gen=[]
-    print(z[0:3])
-return z
+    for i in range(len(z)):
+        gen.append(z.pop(-1))
+        z.insert(0,gen[0])
+        gen=[]
+        print(z[0:3])
+    return z
