4fdd789e2adc73dcb9b82399fa9643ada9ea0798
UnknownC3PO <80990115+UnknownC3PO@users.noreply.github.com>
2021-08-12 23:12:02 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,20 @@
+import json
+import requests
 def lambda_handler(event,context):
-  return 'Hello AWS'
+    base_id="app1yGHzBsgY0VVBd"
+    table_name="MainTable"
+    api_key="key4jDdoqZmxtIjxP"
+    url = "https://api.airtable.com/v0/"+base_id+"/"+table_name+"?api_key="+api_key
+    response=requests.get(url)
+    airtable_response = response.json()
+    z=[]
+    for i in airtable_response["records"]:
+        z.append(i["fields"]["title"])
+    print(z)
+    gen=[]
+    for i in range(len(z)):
+        gen.append(z.pop(-1))
+        z.insert(0,gen[0])
+        gen=[]
+        print(z[0:3])
+    return z
