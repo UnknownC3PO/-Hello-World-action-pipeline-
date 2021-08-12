f2321dad6bed0091c0e26dfb1377c935e4c5d9a2
UnknownC3PO <80990115+UnknownC3PO@users.noreply.github.com>
2021-08-13 00:15:46 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,20 +1,20 @@
 import json
-from botocore.vendored import requests
-def lambda_handler(event,context):
-    base_id="app1yGHzBsgY0VVBd"
-    table_name="MainTable"
-    api_key="key4jDdoqZmxtIjxP"
-    url = "https://api.airtable.com/v0/"+base_id+"/"+table_name+"?api_key="+api_key
-    response=requests.get(url)
-    airtable_response = response.json()
-    z=[]
-    for i in airtable_response["records"]:
-        z.append(i["fields"]["title"])
-    print(z)
+import urllib3
+base_id="app1yGHzBsgY0VVBd"
+table_name="MainTable"
+api_key="key4jDdoqZmxtIjxP"
+url = "https://api.airtable.com/v0/"+base_id+"/"+table_name+"?api_key="+api_key
+http = urllib3.PoolManager()
+r = http.request('GET', url)
+airtable_response = json.loads(r.data.decode('utf-8'))
+z=[]
+for i in airtable_response["records"]:
+    z.append(i["fields"]["title"])
+print(z)
+gen=[]
+for i in range(len(z)):
+    gen.append(z.pop(-1))
+    z.insert(0,gen[0])
     gen=[]
-    for i in range(len(z)):
-        gen.append(z.pop(-1))
-        z.insert(0,gen[0])
-        gen=[]
-        print(z[0:3])
-    return z
+    print(z[0:3])
+return z
