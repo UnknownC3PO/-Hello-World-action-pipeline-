0679ddf0a383e1c6256ea9f8e4b3fc3c278dcabd
UnknownC3PO <80990115+UnknownC3PO@users.noreply.github.com>
2021-08-13 21:40:01 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -13,11 +13,12 @@ for i in airtable_response["records"]:
     z.append(i["fields"]["title"])
 print(z)
 gen=[]
-def lambda_handler(main,context):
+def lambda_handler(event, context):
     while time.time()!=0:
         for i in range(len(z)):
             gen.append(z.pop(-1))
             z.insert(0,gen[0])
             gen=[]
             time.sleep(1)
-            return z[0:3]
+            print(z[0:3])
+            return z
