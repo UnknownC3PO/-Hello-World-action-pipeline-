10c68f63259d73d5c90ad14e282f901be2ca510e
UnknownC3PO <80990115+UnknownC3PO@users.noreply.github.com>
2021-08-14 00:20:48 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,21 +1,18 @@
 import time
 import json
 import urllib3
-base_id="app1yGHzBsgY0VVBd"
-table_name="MainTable"
-api_key="key4jDdoqZmxtIjxP"
-url = "https://api.airtable.com/v0/"+base_id+"/"+table_name+"?api_key="+api_key
-http = urllib3.PoolManager()
-r = http.request('GET', url)
-airtable_response = json.loads(r.data.decode('utf-8'))
-z=[]
-for i in airtable_response["records"]:
-    z.append(i["fields"]["title"])
+import os
 def lambda_handler(event, context):
-    while time.time()!=0:
-        gen=[]
-        gen.append(z.pop(-1))
-        z.insert(0,gen[0])
-        gen=[]
-        time.sleep(1)
-        return z[0:3]
+    api_key = os.getenv("API_KEY",None)
+    base_id = "app1yGHzBsgY0VVBd"
+    table_name = "MainTable"
+    base_url = "https://api.airtable.com/v0"
+    url = f"{base_url}/{base_id}/{table_name}"
+    http = urllib3.PoolManager()
+    r = http.request('GET', url, 
+                    fields={"api_key":api_key})
+    airtable_response = json.loads(r.data.decode('utf-8'))
+    z = []
+    for record in airtable_response["records"]:
+        z.append(record["fields"]["title"])
+    return z[time.time()%len(z)]
