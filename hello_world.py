0246561a0465c4dbc9423bbebc415856871005ce
UnknownC3PO <80990115+UnknownC3PO@users.noreply.github.com>
2021-08-17 20:05:41 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -11,9 +11,9 @@ def lambda_handler(event, context):
     http = urllib3.PoolManager()
     r = http.request('GET', url, 
                     fields={"api_key":api_key})
-    airtable_response = json.loads(r.data)
+    airtable_response = json.loads(r.data.decode('utf-8'))
     try:
         z=[i["title"] for i in (sorted([i["fields"] for i in airtable_response["records"]],key=lambda i:i["ID"]))]
-        return z[int(time.time())%len(z):]+z[:int(time.time())%len(z)]
+        return (z[int(time.time())%len(z):]+z[:int(time.time())%len(z)]).data.decode('utf-8')
     except:
         return 'z is empty'
